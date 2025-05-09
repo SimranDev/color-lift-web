05a4c7f65daabe7301dcedea0fa01d1fcd0d4167
Simran <150635196+simran-cropsy@users.noreply.github.com>
2025-05-09 21:22:30 +1200

    feat: initialize Vue application with main App component

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <Navbar />
+    <main>
+      <Hero />
+      <Features />
+      <Palettes />
+      <HowItWorks />
+      <CTA />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+import Features from "./components/Features.vue";
+import Palettes from "./components/Palettes.vue";
+import HowItWorks from "./components/HowItWorks.vue";
+import CTA from "./components/CTA.vue";
+import Footer from "./components/Footer.vue";
+</script>
